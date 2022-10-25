d39da226ea0833de058d2d5f5322b12e062c1940
student <student@fit.ba>
2022-10-25 14:01:27 +0200

    added project setup and txt file dev1

---
+++ dev1/dev1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev1
+{
+    public class Class1
+    {
+        public int class1_id { get; set; }
+        public string Name { get; set; }
+        public int Surname { get; set; }
+    }
+}
